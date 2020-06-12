0dc732b3d1be629a501690a8e590d5790904f6c1
Наталья Мельничук <tyssia33@yandex.ru>
2020-06-12 23:54:31 +0300

    12.06.2020 Version

---
+++ js/modal-form.js
@@ -0,0 +1,31 @@
+var buttonContact = document.querySelector(".button-contact");
+var modalForm = document.querySelector(".modal-form");
+var modalClose = modalForm.querySelector(".modal-close");
+var userName = modalForm.querySelector(".user-name");
+var userEmail = modalForm.querySelector(".user-email");
+var writeUs = modalForm.querySelector(".write-us");
+var userText = modalForm.querySelector(".user-text");
+buttonContact.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	modalForm.classList.add("modal-show");
+	userName.focus();
+});
+modalClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalForm.classList.remove("modal-show");
+});
+writeUs.addEventListener("submit", function (evt) {
+  if (!userName.value || !userEmail.value || userText.value  ) {
+    evt.preventDefault();
+}else {
+    localStorage.setItem("name", userName.value);
+  }
+});
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("name");
+} catch (err) {
+  isStorageSupport = false;
+}
